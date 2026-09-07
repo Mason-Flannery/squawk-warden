a80038087483dabcd89983aff2bf5310641ddb58
mflannery <mbubbalou17@gmail.com>
2026-09-06 22:41:00 -0400

    Rewrite handlers to use new new database handlers

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,32 +1,51 @@
-use std::{sync::Arc};
-
+pub mod db;
 use axum::{
-    Json, Router, extract::State, routing::{get, post}
+    Json, Router,
+    extract::State,
+    routing::{get, post},
 };
+use dotenvy::dotenv;
 use shared::Reading;
-use tokio::sync::Mutex;
+use sqlx::SqlitePool;
+
 #[tokio::main]
 async fn main() {
-    let latest = Arc::new(Mutex::new(Reading::default())); 
+    let _ = dotenv(); // Load environment variables 
+
+    let latest = db::init().await;
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
         .route("/readings/latest", get(latest_handler))
         .route("/readings/submit", post(submit_handler))
-        .with_state(latest.clone());
+        .with_state(latest);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap(); // TODO: Bind to server port defined in config.toml?
 
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn latest_handler(State(state): State<Arc<Mutex<Reading>>>) -> Json<Reading> {
-    let reading = state.lock().await;
-    // format!("Temperature: {}F, Relative Humidity: {}%", reading.temperature*1.8+32.0, reading.humidity)
-    Json(reading.clone())
+async fn latest_handler(State(state): State<SqlitePool>) -> Json<Reading> {
+    if let Ok(Some(reading)) = db::get_latest(&state).await {
+        Json(Reading {
+            temperature: reading.temperature,
+            humidity: reading.humidity,
+        })
+    } else {
+        // Lazy approach, need to propagate errors once frontend is skeletonized
+        Json(Reading {
+            temperature: -1.0,
+            humidity: -1.0,
+        })
+    }
 }
 #[axum::debug_handler]
-async fn submit_handler(State(state): State<Arc<Mutex<Reading>>>, Json(payload): Json<Reading>) {
-    let mut reading = state.lock().await;
-    *reading = payload;
-    println!("New reading is: {} {}", reading.temperature, reading.humidity); 
+async fn submit_handler(State(state): State<SqlitePool>, Json(payload): Json<Reading>) {
+    if let Err(result) = db::new_reading(&state, payload.temperature, payload.humidity).await {
+        println!("An error occurred writing to disk: {}", result);
+    } else {
+        println!(
+            "New reading is: {} {}",
+            payload.temperature, payload.humidity
+        );
+    }
 }
